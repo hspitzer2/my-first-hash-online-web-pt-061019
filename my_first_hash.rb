a92dae966a511c10bccdf14a5db652d435fa77d5
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-24 01:02:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,43 +1,39 @@
+
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  family = { "husband" =>"John", "daugther" =>"Jules", "son" => "Evan"}
 end
 
+def shipping_manifest(mainfest)
+  the_manifest = { "whales"=> 5, "porcelain vases" => 2, "oil paintings" => 3 }
+ end
+shipping_manifest[]
 
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
-end
-
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-end
+#
+# def retrieval
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5,
+#                   "porcelain vases" => 2,
+#                   "oil paintings" => 3
+#                  }
+#
+#   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+#
+# end
+#
+# def adding
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5,
+#                   "porcelain vases" => 2,
+#                   "oil paintings" => 3
+#                  }
+#   # add 2 muskets to the shipping_manifest hash below
+#
+#
+#
+#   # add 4 gun powder to the shipping_manifest hash below
+#
+#
+#
+#   # return the shipping_manifest hash below
+#
+# end
