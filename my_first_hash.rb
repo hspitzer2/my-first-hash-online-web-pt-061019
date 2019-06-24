0331bf7a62e73197e52837ad5918d0e2b9a831ed
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-24 01:25:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ end
 
 def adding
 shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-shipping_manifest<<["muskets" => 2] 
+shipping_manifest<<["muskets" => 2]
 shipping_manifest<<["gun powder" => 4]
 end
 shipping_manifest
